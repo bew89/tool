f05b436d1a3525203d39555bdfd442ef8be670c6
Ben <benwoodfield21@icloud.com>
2024-02-18 22:33:12 +0000

    all working i suppose

--- arithmetic/arithmetic.js
+++ arithmetic/arithmetic.js
@@ -2,26 +2,27 @@
 
 
 
-const firstNumber = document.getElementById("numOne").value;
-const secondNumber = document.getElementById("numTwo")/value;
-const operator;
-const result;
 
-operator();
 
-out();
 
-function out(operator, result){
+function out(firstNumber, secondNumber, operator, result){
 
 document.getElementById("output").innerText = 
 `${firstNumber} ${operator} ${secondNumber} = ${result}`;
 	
 }	
-
-function operator(){
+function rootOut(rootNumber, operator,result){
 	
-	if firstNumber != Number || secondNumber != Number){
-		return alert("Enter a number please
+	document.getElementById("rootOutput").innerText = 
+`${operator} ${rootNumber} = ${result}`;
+
+}
+
+
+function checkNum(firstNumber,secondNumber){
+	
+	if (isNaN(firstNumber) || isNaN(secondNumber)){
+		return alert("Enter a number please");
 	
 	}
 	
@@ -29,35 +30,79 @@ function operator(){
 
 function addition(){
 	
+const firstNumber = parseFloat(document.getElementById("numOne").value);
+const secondNumber = parseFloat(document.getElementById("numTwo").value);
+
+checkNum(firstNumber,secondNumber);
+
 let operator = "+";
 let result = (firstNumber + secondNumber);	
-	out(operator,result);
+
+
+out(firstNumber, secondNumber, operator,result);
 }
 
 function subtraction(){
-	
+	const firstNumber = parseFloat(document.getElementById("numOne").value);
+const secondNumber = parseFloat(document.getElementById("numTwo").value);
+
+checkNum(firstNumber,secondNumber);
+
 let operator = "-";
 let result = (firstNumber - secondNumber);	
 
+out(firstNumber, secondNumber, operator,result);
 }
 
 function multiplication(){
-	
+	const firstNumber = parseFloat(document.getElementById("numOne").value);
+const secondNumber = parseFloat(document.getElementById("numTwo").value);
+
+checkNum(firstNumber,secondNumber);
+
 let operator = "*";
 let result = (firstNumber * secondNumber);	
-	
+
+out(firstNumber, secondNumber, operator,result);	
 }
 
 function division(){
-	
+	const firstNumber = parseFloat(document.getElementById("numOne").value);
+const secondNumber = parseFloat(document.getElementById("numTwo").value);
+
+checkNum(firstNumber,secondNumber);
+
 let operator = "/";
 let result = (firstNumber / secondNumber);	
-	
+
+out(firstNumber, secondNumber, operator,result);	
+}
+
+function root(){
+const rootNumber = parseFloat(document.getElementById("rootNum").value);
+
+if(isNaN(rootNumber)){
+	return alert("Enter a number please");
+}
+
+let operator = "√";
+let result = (rootNumber**0.5);	
+
+rootOut(rootNumber, operator,result);	
 }
 
 function modulo(){
-	
+const firstNumber = parseFloat(document.getElementById("numOne").value);
+const secondNumber = parseFloat(document.getElementById("numTwo").value);
+
+
+checkNum(firstNumber,secondNumber);
+
 let operator = "%";
 let result = (firstNumber % secondNumber);	
-	
+
+out(firstNumber, secondNumber, operator,result);	
 }
+
+
+
