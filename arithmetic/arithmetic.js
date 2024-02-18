37a1cd2656ba7224861214c79b28a49588b3e80f
Ben <benwoodfield21@icloud.com>
2024-02-18 19:54:14 +0000

    added more arithmetic

--- arithmetic/arithmetic.js
+++ arithmetic/arithmetic.js
@@ -2,6 +2,62 @@
 
 
 
+const firstNumber = document.getElementById("numOne").value;
+const secondNumber = document.getElementById("numTwo")/value;
+const operator;
+const result;
 
+operator();
 
-function
+out();
+
+function out(){
+
+document.getElementById("output".innerText = 
+firstNumber	+ " " + operator + " " + secondNumber + " = " + result;
+	
+}	
+
+function operator(){
+	
+	if firstNumber != Number || secondNumber != Number){
+		return alert("Enter a number please
+	
+	}
+	
+}
+
+function addition(){
+	
+let operator = "+";
+let result = (firstNumber + secondNumber);	
+	
+}
+
+function subtraction(){
+	
+let operator = "-";
+let result = (firstNumber - secondNumber);	
+
+}
+
+function multiplication(){
+	
+let operator = "*";
+let result = (firstNumber * secondNumber);	
+	
+}
+
+function division(){
+	
+let operator = "/";
+let result = (firstNumber / secondNumber);	
+	
+}
+
+function modulo(){
+	
+let operator = "%";
+let result = (firstNumber % secondNumber);	
+	
+}
