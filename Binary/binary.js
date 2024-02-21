0bb50b147949f327a5fa071f653ab76e2df84ffb
Ben <benwoodfield21@icloud.com>
2024-02-21 15:47:38 +0000

    updates
    Updated links and continued with signed mag for binary, no luck so far

--- Binary/binary.js
+++ Binary/binary.js
@@ -1,24 +1,6 @@
 //if ones complemetn is ticked then do stuff
 //if twors is ticked do stuff
 
-function checkIfTicked(){
-if(Checkbox.checked){
-	
-	if(Checkbox.id === 'onesComp'){
-		return "ones";
-		
-	}else if(Checkbox.id === 'twosComp'){
-		return "twos";
-		
-	}else {
-		return 0;
-	
-	
-	
-}
-}
-}
-
 
 
 
@@ -28,29 +10,70 @@ if(Checkbox.checked){
 //-----------------------------------------------------------------------------------
 
 function decimalToBinary(){
-	
+
+defaultToBinary();
+signedToBinary();
+onesCompToBinary();
+twosCompToBinary();
+
+}
+
+let binary = "";
+function defaultToBinary(){	
 let userText = parseFloat(document.getElementById("decimalText").value);
 
 if(isNaN(userText)){
 return alert("Enter a number. Text will not work");	
 }
 
-
-
-
 let binaryText = userText.toString(2);
 let padding = getPadding(binaryText);
 
-
-let binary = "0".repeat(padding - binaryText.length);
+binary = "0".repeat(padding - binaryText.length);
 
 binary += binaryText;
 
-document.getElementById("binaryOutput").innerText = binary;
-
+document.getElementById("defaultBinaryOutput").innerText = binary;
+console.log("firs step");
 }
 
 //-----------------------------------------------------------------------------------
+
+function signedToBinary(){
+	
+let userText = parseFloat(document.getElementById("decimalText").value);
+	
+let signedBinary = binary;
+if(userText < 0){
+
+signedBinary[0] = '1';
+
+}else{
+signedBinary[0] = '0';
+	
+}
+document.getElementById("signedBinaryOutput").innerText = signedBinary;
+console.log("I got here");
+}
+
+//-----------------------------------------------------------------------------------
+
+/*
+function onesCompToBinary(){
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+function twosCompToBinary(){
+	
+	
+}
+
+//-----------------------------------------------------------------------------------
+
+*/
+
 
 function binaryToDecimal(){
 	
@@ -64,7 +87,7 @@ let sign = checkIfTicked();
 
 let decimalText = parseInt(userInput,2);
 
-document.getElementById("decimalOutput").innerText = decimalText;
+document.getElementById("defaultDecimalOutput").innerText = decimalText;
 }
 
 //-----------------------------------------------------------------------------------
