a9313478873bf0a76958d256d877bf4140066bb7
Ben <benwoodfield21@icloud.com>
2024-03-03 18:23:22 +0000

    overhaul
    Designed the css for the game and created 3 menus for it to be separated, added automated stardust collection

--- idlegame/idlegame.js
+++ idlegame/idlegame.js
@@ -1,4 +1,74 @@
 
+function showMainSection(){
+	
+	let mainSection = document.getElementsByClassName("mainDisplaySection");
+	for(let k = 0; k < mainSection.length; k++){
+		mainSection[k].style.display = "block";
+	}
+	
+	let planetSection = document.getElementsByClassName("planetDisplaySection");
+	for(let k = 0; k < planetSection.length; k++){
+		planetSection[k].style.display = "none";
+	}
+	
+	let galaxySection = document.getElementsByClassName("galaxyDisplaySection");
+	for(let k = 0; k < galaxySection.length; k++){
+		galaxySection[k].style.display = "none";
+	}
+	refreshItemDisplay();
+}
+function showPlanetSection(){
+	/*Here I get all the elements of the main class and hide them*/
+	let mainSection = document.getElementsByClassName("mainDisplaySection");
+	for(let k = 0; k < mainSection.length; k++){
+		mainSection[k].style.display = "none";
+	}
+	
+	let planetSection = document.getElementsByClassName("planetDisplaySection");
+	for(let k = 0; k < planetSection.length; k++){
+		planetSection[k].style.display = "block";
+	}
+	
+	let galaxySection = document.getElementsByClassName("galaxyDisplaySection");
+	for(let k = 0; k < galaxySection.length; k++){
+		galaxySection[k].style.display = "none";
+	}
+	refreshItemDisplay();
+}
+function showGalaxySection(){
+	
+		let mainSection = document.getElementsByClassName("mainDisplaySection");
+	for(let k = 0; k < mainSection.length; k++){
+		mainSection[k].style.display = "none";
+	}
+	
+	let planetSection = document.getElementsByClassName("planetDisplaySection");
+	for(let k = 0; k < planetSection.length; k++){
+		planetSection[k].style.display = "none";
+	}
+	
+	let galaxySection = document.getElementsByClassName("galaxyDisplaySection");
+	for(let k = 0; k < galaxySection.length; k++){
+		galaxySection[k].style.display = "block";
+	}
+	refreshItemDisplay();
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class item {
 	
 	constructor(name, amount, age){
@@ -16,34 +86,57 @@ class item {
 	}
 }
 
-
-
-
-
 let stardustAmount = 0;
 let starshardAmount = 0;
 let i = 0;
 let j = 0;
-let collectionLevel = 1;
-let upgradePrice = [100,250,500,1000,1500,3000,5000,7500,10000];
-let stardustCollectUpgradePrice = 0;
+/*
+let stardustGainLevel = 1;
+let stardustUpgradePrice = [100,250,500,1000,1500,3000,5000,7500,10000];
+let stardustGainUpgradeIndex = 0;
+
+let stardustAutoGainLevel = 1;
+let stardustAutoUpgradePrice = [];
+let stardustAutoGainUpgradeIndex = 0;
+*/
+let stardust = {
+    gainLevel: 1,
+    upgradePrice: [100,250,500,1000,1500,3000,5000,7500,10000],
+    gainUpgradeIndex: 0
+};
+
+let stardustAuto = {
+    gainLevel: 0,
+    upgradePrice: [10,25,50,85,125,200,500,750,1000],
+    gainUpgradeIndex: 0
+};
+
+
+function refreshItemDisplay(){
+	
+	document.getElementById("stardustAmount").innerText = `Stardust Amount: ${stardustAmount}`;
+	document.getElementById("starshardAmount").innerText = `Starshard Amount: ${starshardAmount}`;
+	
+}
+
 function barIncrease(){
 	getStarshard();
 		document.getElementById("notEnoughStardust").innerText = "";
-	i = i + collectionLevel;
-	//console.log(collectionLevel + " " + i);
-	stardustAmount = stardustAmount + collectionLevel;
+	i = i + stardust.gainLevel ;
+
+	stardustAmount = stardustAmount + stardust.gainLevel ;
 	
-	document.getElementById("stardustAmount").innerText = `Stardust Amount: ${stardustAmount}`;
+	refreshItemDisplay();
+	
 	let bar = document.getElementById("theProgress");
 	
 
 let delay = setInterval(function(){	
 if(j > 100 || j === i || i >= 100){
 	clearInterval(delay);
-	console.log("in the if");
+	;
 }else{
-	console.log("in the else");
+	
 	j = j + 0.5;
 	bar.style.width = j + "%";
 }
@@ -58,21 +151,21 @@ if(j > 100 || j === i || i >= 100){
 		console.log(bar.style.width);	
 }
 
-function upgradeCollection(){
+function upgradeStardustGain(){
 	
-		if(stardustAmount >= upgradePrice[stardustCollectUpgradePrice]){
+		if(stardustAmount >= stardust.upgradePrice[stardust.gainUpgradeIndex]){
 	
-			stardustAmount = stardustAmount - upgradePrice[stardustCollectUpgradePrice];
+			stardustAmount = stardustAmount - stardust.upgradePrice[stardust.gainUpgradeIndex];
 		
-			collectionLevel += collectionLevel;
+			stardust.gainLevel  += stardust.gainLevel;
 	
-			document.getElementById("stardustAmount").innerText = `Stardust Amount: ${stardustAmount}`;
+				refreshItemDisplay();
 		
-			stardustCollectUpgradePrice += 1;
+			stardust.gainUpgradePrice += 1;
 	
 		}else{
 			
-			document.getElementById("notEnoughStardust").innerText = `You do not have enough stardust to upgrade, you need: ${upgradePrice[stardustCollectUpgradePrice] - stardustAmount} more stardust`;
+			document.getElementById("notEnoughStardust").innerText = `You do not have enough stardust to upgrade, you need: ${stardust.upgradePrice[stardust.gainUpgradeIndex] - stardustAmount} more stardust`;
 		}
 	
 }
@@ -95,3 +188,24 @@ let dropChance = Math.random();
 	
 	
 }
+
+function autoUpgrade(){
+
+if(stardustAmount >= stardustAuto.upgradePrice[stardustAuto.gainUpgradeIndex]){
+	stardustAmount -= stardustAuto.upgradePrice[stardustAuto.gainUpgradeIndex];
+
+	refreshItemDisplay();
+	stardustAuto.gainLevel += 1;
+	stardustAuto.gainUpgradeIndex++;
+	console.log(stardustAuto.gainUpgradeIndex);
+}else{
+	document.getElementById("notEnoughStardust").innerText = `You do not have enough stardust to upgrade, you need: ${stardustAuto.upgradePrice[stardustAuto.gainUpgradeIndex] - stardustAmount} more stardust`;
+}
+}
+let autoInterval = setInterval(function(){
+	
+	stardustAmount += stardustAuto.gainLevel;
+		
+			refreshItemDisplay();
+			
+},5000);
