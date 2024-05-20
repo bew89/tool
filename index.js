a58c7724f90b2d3756545699f286330283c86cb0
Ben <benwoodfield21@icloud.com>
2024-05-20 01:14:10 +0100

    added some animations

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+enableLinks();
+
+function enableLinks() {
+    document.addEventListener('DOMContentLoaded', () => {
+        const links = document.querySelectorAll('#links a');
+        links.forEach(link => {
+            link.addEventListener('click', (event) => {
+                event.preventDefault(); // Prevent the default action
+                console.log(`Link clicked: ${link.href}`);
 
 
+                linkClick(link);
+                setTimeout(() => {
+                   window.location.href = link.href;
+                }, 1000);
+            });
+        })
+    });
+}
 
-<form>
-<p> What is your name?
-<input type="text" name="name" size="10" />
-</p>
-</form>
+function linkClick(link) {
+    link.classList.add('clickedLink');
+
+    console.log("started animation")
+    setTimeout(() => {
+        link.classList.remove('clickedLink');
+
+    }, 1000)
+}
